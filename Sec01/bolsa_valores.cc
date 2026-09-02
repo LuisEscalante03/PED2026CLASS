495085be26f4c7f6b667ec2265bb78b54b2fc642
Luis Escalante <00055726@uca.edu.sv>
2026-09-02 12:13:35 -0600

    Actualice bolsa_valores.cc

--- Sec01/bolsa_valores.cc
+++ Sec01/bolsa_valores.cc
@@ -17,6 +17,10 @@ void CalcularFluctuacionAccion(Accion *accion);
 
 int main() {
 
+    struct Accion accion;
+
+    CalcularFluctuacionAccion(
+
 }
 
 float CalcularMargenGanancia(float precio_compra, float precio_venta){
@@ -24,6 +28,20 @@ float CalcularMargenGanancia(float precio_compra, float precio_venta){
 }
 
 void CalcularFluctuacionAccion(Accion &accion){
-    //Generar valor aleatorio entre 10 y 500
-    accion.ultimo_precio = 10 + (rand() % 500);
+    //Generar valor aleatorio entre 10.0 y 500.0
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<double> dist_precio(10.0, 500.0);
+    return dist_precio(gen);
+
+    accion.precio_compra = dist_precio(gen);
+}
+
+void CalcularFluctuacionAccion(Accion *accion){
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<double> dist_precio(10.0, 500.0);
+    return dist_precio(gen);
+
+    accion.precio_compra = dist_precio(gen);
 }
